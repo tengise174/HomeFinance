f203d50bd055f3fe08ac827f20e8adf2ac4f08a6
tengise174 <117653225+tengise174@users.noreply.github.com>
2024-07-01 02:19:36 -0500

    uiContoller function nemsen

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function () {
 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
 })();
 
 
@@ -12,10 +32,11 @@ var financeController = (function () {
 // Програмын холбогч контроллер
 var appController = (function (uiController, fnController) {
 
+    var DOM = uiController.getDOMstrings();
 
     var crtlAddItem = function () {
         // 1. Оруулах өгөгдлийг дэлгэцээс олж авна. 
-
+        console.log(uiController.getInput());
 
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерд дамжуулж тэнд хадгална. 
 
@@ -27,7 +48,7 @@ var appController = (function (uiController, fnController) {
 
     };
 
-    document.querySelector('.add__btn').addEventListener('click', function () {
+    document.querySelector(DOM.addBtn).addEventListener('click', function () {
         crtlAddItem();
     });
 
