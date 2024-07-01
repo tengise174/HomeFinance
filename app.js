23f4983e671f88a80dba4aaa950f1b54b6fa37b8
tengise174 <117653225+tengise174@users.noreply.github.com>
2024-07-01 02:58:32 -0500

    Income Expenses class zarlasan

--- app.js
+++ app.js
@@ -27,6 +27,30 @@ var uiController = (function () {
 // Санхүүтэй ажиллах контроллер
 var financeController = (function () {
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    }
+
 })();
 
 
