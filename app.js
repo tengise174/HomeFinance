cd46b99b3cd59012a47bd95e0ec306ef7a0ddc45
tengise174 <117653225+tengise174@users.noreply.github.com>
2024-07-02 03:18:28 -0500

    display income and expenses on screen

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function () {
-
     var DOMstrings = {
         inputType: ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
-        addBtn: ".add__btn"
+        addBtn: ".add__btn",
+        incomeList: ".income__list",
+        expenseList: ".expenses__list"
+
     };
 
     return {
         getInput: function () {
             return {
-                type: document.querySelector(DOMstrings.inputType).value,
+                type: document.querySelector(DOMstrings.inputType).value, // exp, inc
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
@@ -19,26 +21,65 @@ var uiController = (function () {
 
         getDOMstrings: function () {
             return DOMstrings;
+        },
+
+        clearFields: function () {
+            var fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+
+            //Convert list to array
+            var fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (el, index, array) {
+                el.value = "";
+            });
+
+            // for (var i = 0; i < fields.length; i++) {
+            //     fieldsArr[i].value = "";
+            // }
+
+            fieldsArr[0].focus();
+        },
+
+        addListItem: function (item, type) {
+            /*орлого зарлагын элементийг агуулсан html-ийг бэлтгэнэ.*/
+            var html, list;
+            if (type === "inc") {
+                list = DOMstrings.incomeList;
+                html =
+                    '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+            } else {
+                list = DOMstrings.expenseList;
+                html =
+                    '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            //Тэр HTML дотроо орлого зарлагын утгуудыг REPLACE хийж өгнө үү.
+            html = html.replace("%id%", item.id);
+            html = html.replace("$$DESCRIPTION$$", item.description);
+            html = html.replace("$$VALUE$$", item.value);
+
+            //Бэлтгэсэн HTML -ээ DOM-руу хийж өгнө.
+            document.querySelector(list).insertAdjacentHTML("beforeend", html);
         }
     };
 })();
 
-
 // Санхүүтэй ажиллах контроллер
 var financeController = (function () {
-
+    // private data
     var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
 
+    // private data
     var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
 
+    // private data
     var data = {
         items: {
             inc: [],
@@ -53,42 +94,46 @@ var financeController = (function () {
 
     return {
         addItem: function (type, desc, val) {
-
-            var item; id;
-
-            // identification 
+            var item, id;
 
             if (data.items[type].length === 0) id = 1;
             else {
                 id = data.items[type][data.items[type].length - 1].id + 1;
             }
 
-            if (type === 'inc') {
-                item - new Income(id, desc, val);
+            if (type === "inc") {
+                item = new Income(id, desc, val);
             } else {
-                // type === esc
-                item - new Expense(id, desc, val);
+                item = new Expense(id, desc, val);
             }
 
             data.items[type].push(item);
+
+            return item;
+        },
+
+        seeData: function () {
+            return data;
         }
     };
-
 })();
-
 
 // Програмын холбогч контроллер
 var appController = (function (uiController, financeController) {
-
-    var crtlAddItem = function () {
-        // 1. Оруулах өгөгдлийг дэлгэцээс олж авна. 
+    var ctrlAddItem = function () {
+        // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
         var input = uiController.getInput();
 
-        console.log(input);
-        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерд дамжуулж тэнд хадгална. 
-        financeController.addItem(input.type, input.description, input.value);
+        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерд дамжуулж тэнд хадгална.
+        var item = financeController.addItem(
+            input.type,
+            input.description,
+            input.value
+        );
 
-        // 3. Олж авсан өгөгдлүүдийг вэб дээр тохирох хэсгүүдэд гаргана. 
+        // 3. Олж авсан өгөгдлүүдийг вэб дээр тохирох хэсгүүдэд гаргана.
+        uiController.addListItem(item, input.type);
+        uiController.clearFields();
 
         // 4. Төсвийг тооцоолно.
 
@@ -97,28 +142,25 @@ var appController = (function (uiController, financeController) {
     };
 
     var setupEventListeners = function () {
-
         var DOM = uiController.getDOMstrings();
 
-        document.querySelector(DOM.addBtn).addEventListener('click', function () {
-            crtlAddItem();
+        document.querySelector(DOM.addBtn).addEventListener("click", function () {
+            ctrlAddItem();
         });
 
-
-        document.addEventListener('keypress', function (event) {
+        document.addEventListener("keypress", function (event) {
             if (event.keyCode === 13 || event.which === 13) {
-                crtlAddItem();
+                ctrlAddItem();
             }
         });
     };
 
     return {
         init: function () {
-            console.log('Application started ...');
+            console.log("Application started...");
             setupEventListeners();
         }
     };
-
 })(uiController, financeController);
 
 appController.init();
