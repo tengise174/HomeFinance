0863df567fa28bc6fa85250ad624f3ab67c25210
tengise174 <117653225+tengise174@users.noreply.github.com>
2024-07-04 04:53:11 -0500

    delete list items on display

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var uiController = (function () {
         tusuvLabel: ".budget__value",
         incomeLabel: ".budget__income--value",
         expenseLabel: ".budget__expenses--value",
-        percentageLabel: ".budget__expenses--percentage"
+        percentageLabel: ".budget__expenses--percentage",
+        containerDiv: ".container"
     };
 
     return {
@@ -62,11 +63,11 @@ var uiController = (function () {
             if (type === "inc") {
                 list = DOMstrings.incomeList;
                 html =
-                    '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+                    '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
             } else {
                 list = DOMstrings.expenseList;
                 html =
-                    '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
+                    '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
             //Тэр HTML дотроо орлого зарлагын утгуудыг REPLACE хийж өгнө үү.
             html = html.replace("%id%", item.id);
@@ -75,6 +76,11 @@ var uiController = (function () {
 
             //Бэлтгэсэн HTML -ээ DOM-руу хийж өгнө.
             document.querySelector(list).insertAdjacentHTML("beforeend", html);
+        },
+
+        deleteListItem: function (id) {
+            var el = document.getElementById(id);
+            el.parentNode.removeChild(el);
         }
     };
 })();
@@ -147,6 +153,18 @@ var financeController = (function () {
             }
         },
 
+        deleteItem: function (type, id) {
+            var ids = data.items[type].map(function (el) {
+                return el.id;
+            });
+
+            var index = ids.indexOf(id);
+
+            if (index !== 1) {
+                data.items[type].splice(index, 1);
+            }
+        },
+
         addItem: function (type, desc, val) {
             var item, id;
 
@@ -214,6 +232,26 @@ var appController = (function (uiController, financeController) {
             if (event.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
             }
+        });
+
+        document.querySelector(DOM.containerDiv).addEventListener('click', function (event) {
+            var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+            if (id) {
+                var arr = id.split('-');
+                var type = arr[0];
+                var itemId = parseInt(arr[1]);
+
+                // 1. Санхүүгийн модулиас type, id ашиглаад устгана.
+                financeController.deleteItem(type, itemId);
+
+                // 2. Дэдгэц дээрээс энэ элементийг устгана. 
+                uiController.deleteListItem(id);
+
+                // 3. Үлдэгдэл тооцоог шинэчилж харуулна.
+
+            }
+
         });
     };
 
